a5a02ab018f21f199126274c716105d6a3efa506
BrendanMcCulloch <22bmcculloch@athenian.org>
2021-01-19 02:57:05 +0000

    made methods but couldn't finish. I will go to C&C

--- src/main/java/MyMain.java
+++ src/main/java/MyMain.java
@@ -1,22 +1,66 @@
 public class MyMain {
 
     // Returns the mean of the 2D array mat
-    public static double mean(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+    public static double mean(double[][] mat) {
+        double c = 0; 
+        for (int row = 0; row < mat.length; row++){
+            for(int col = 0; col < mat[0].length; col++){
+                c = c + mat[row][col];
+                int j = c/(mat.length * mat[0].length);
+            }
+        }
+        return j;
     }
 
     // Returns the median of the 2D array mat
     public static double median(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+        double[] order = new double[mat.length * mat[0].length];
+        int j = 0;
+        int h = 0;
+        for (int row = 0; row < mat.length; row++){
+            for (int col = 0; col < mat[0].length; col++){
+                 order[j] = mat[row][col];
+                 j++;
+                 
+            }
+        }
+        for (int i = 0; i < order.length; i++){
+            if (order[h] > order[h+1]){
+                h++;
+                return order[order.length/2];
+            }
+            if (order[h] < order[h+1]){
+                order[h+1] = order[h];
+                order[h] = order[h+1];
+                h++;
+                return order[order.length/2];
+            }
+
+            
+        }
     }
     
 
     // Returns the mode of the 2D array mat
-    public static double mode(double[][] mat) { 
-        // YOUR CODE HERE
-        return -1.0;
+    public static double mode(double[][] mat) {
+        double[] single = new double[mat.length * mat[0].length];
+        int c = 0;
+        int j = 0;
+        for (int row = 0; row < mat.length; row++){
+            for (int col = 0; col < mat[0].length; col++){
+                single[c] = mat[row][col];
+                c++;
+
+            }
+
+        }
+        for (int i = 0; i < single.length; i++){
+            if (single[i] = single[i+1]){
+                j++;
+            }
+
+        } 
+        return j;
     }
 
 
